9a3d0cef38a5422dfc5e6ab669eb18d7059ca10a
Mark Johnston <mjstone587@Yahoo.com>
2022-03-26 11:27:37 -0500

    changed print message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send("Hi There");
 });
 
 const PORT = process.env.PORT || 5000;
