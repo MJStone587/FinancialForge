e89dd43db6c531b1e33015df985f82d8e9dc78ea
Mark Johnston <mjstone587@Yahoo.com>
2022-08-11 11:46:03 -0500

    Mime mismatch content sniff troubleshoot

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname + "/public")));
 app.use(flash());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   require("express-session")({
